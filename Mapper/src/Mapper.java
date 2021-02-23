abbf5448744f28edd4a64f1f8f118b087b400d81
Filipe Ferreira <filipesferreira94@gmail.com>
2021-02-23 02:04:36 +0000

    Add two new methods FillDataTableOfFiles; DivideObjectsFilesFromTotalMappers;

--- Mapper/src/Mapper.java
+++ Mapper/src/Mapper.java
@@ -8,4 +8,19 @@ public class Mapper extends UnicastRemoteObject implements MapperInterface {
 
     }
 
+    /**
+     * @description Add to the hashmap the respective Id
+     * and Object divided by the total of mappers
+     * @param length
+     */
+    public void FillDataTableOfFiles(int length) {
+        //TODO: add object divided with respective id in the hashmap
+
+
+        //TODO: Call the Reducer
+    }
+
+    public void DivideObjectsFilesFromTotalMappers() {
+
+    }
 }
--- Mapper/src/MapperInterface.java
+++ Mapper/src/MapperInterface.java
@@ -1,5 +1,10 @@
 import java.rmi.Remote;
+import java.rmi.RemoteException;
 
 public interface MapperInterface extends Remote {
+
+    void FillDataTableOfFiles(int length) throws RemoteException;
+
+    void DivideObjectsFilesFromTotalMappers() throws RemoteException;
 
 }
