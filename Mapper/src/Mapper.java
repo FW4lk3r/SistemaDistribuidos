6074b8ed4419aee0f5eb39a3df119f468e66133b
Filipe Ferreira <filipesferreira94@gmail.com>
2021-02-24 21:40:57 +0000

    Add sendReducer()

--- Mapper/src/Mapper.java
+++ Mapper/src/Mapper.java
@@ -1,6 +1,9 @@
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Sets;
 
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.*;
@@ -8,6 +11,7 @@ import java.util.*;
 public class Mapper extends UnicastRemoteObject implements MapperInterface {
 
     private ArrayList<ResourceInfo> Mapper = new ArrayList<>();
+    private static ObjectRegistryInterface objRegInt;
     private Integer port;
 
     protected Mapper(Integer port) throws RemoteException
@@ -20,19 +24,26 @@ public class Mapper extends UnicastRemoteObject implements MapperInterface {
         Mapper = storageData;
     }
 
+    public void sendReducer(ArrayList<Integer> data, String registryAddress) throws RemoteException, NotBoundException, MalformedURLException {
+        objRegInt = (ObjectRegistryInterface) Naming.lookup(registryAddress);
+
+        String portReducer = objRegInt.getObject("2030");
+        ReducerInterface reducerInterface = (ReducerInterface) Naming.lookup(portReducer);
+
+        //reducerInterface.receiveFromStorage(data);
+    }
+
     /**
      * Compute statistics for each combination
      * @param len number of combinations
      * @param fileCount number of files (i.e., runs)
      */
     public Set<Set<ResourceInfo>> combinations(int len, int fileCount) {
-        /*
-            ArrayList<ResourceInfo> resources = new ArrayList<>(Mapper.keySet());
+
+            ArrayList<ResourceInfo> resources = new ArrayList<>(Mapper);
             System.out.println("Number of resources " + resources.size());
             Set<Set<ResourceInfo>> combinations = Sets.combinations(ImmutableSet.copyOf(resources), len);
 
             return combinations;
-        */
-        return null;
     }
 }
--- Mapper/src/MapperInterface.java
+++ Mapper/src/MapperInterface.java
@@ -1,3 +1,5 @@
+import java.net.MalformedURLException;
+import java.rmi.NotBoundException;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.util.ArrayList;
@@ -7,4 +9,6 @@ public interface MapperInterface extends Remote {
 
     void ReceiveStorageFromMaster(ArrayList<ResourceInfo> storageData) throws RemoteException;
     Set<Set<ResourceInfo>> combinations(int len, int fileCount) throws  RemoteException;
+
+    void sendReducer(ArrayList<Integer> data, String registryAddress) throws  RemoteException, NotBoundException, MalformedURLException;
 }
